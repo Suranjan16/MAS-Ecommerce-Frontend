2942a74be9b77d5d7b12684edb6bd6ff3695f872
Suranjan16 <suranjanmaity851@gmail.com>
2026-05-25 20:39:21 +0530

    feat: add logout and dynamic navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,87 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
+
+    const token = localStorage.getItem("token");
+
     const role = localStorage.getItem("role");
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+
+        localStorage.removeItem("token");
+
+        localStorage.removeItem("role");
+
+        alert("Logout successful");
+
+        navigate("/login");
+
+        window.location.reload();
+    };
 
     return (
         <div>
+
             <h2>MAS Ecommerce</h2>
 
             <Link to="/home">Home</Link>
+
             <br />
 
-            <Link to="/cart">Cart</Link>
-            <br />
+            {
+                token && (
+                    <>
+                        <Link to="/cart">
+                            Cart
+                        </Link>
 
-            <Link to="/orders">Orders</Link>
-            <br />
+                        <br />
 
-            {role === "ADMIN" && (
-                <>
-                    <Link to="/admin">Admin Dashboard</Link>
-                    <br />
-                </>
-            )}
+                        <Link to="/orders">
+                            Orders
+                        </Link>
 
-            <Link to="/login">Login</Link>
-            <br />
+                        <br />
+                    </>
+                )
+            }
 
-            <Link to="/signup">Signup</Link>
+            {
+                role === "ADMIN" && (
+                    <>
+                        <Link to="/admin">
+                            Admin Dashboard
+                        </Link>
+
+                        <br />
+                    </>
+                )
+            }
+
+            {
+                !token ? (
+                    <>
+                        <Link to="/login">
+                            Login
+                        </Link>
+
+                        <br />
+
+                        <Link to="/signup">
+                            Signup
+                        </Link>
+                    </>
+                ) : (
+                    <button onClick={handleLogout}>
+                        Logout
+                    </button>
+                )
+            }
 
             <hr />
+
         </div>
     );
 }
